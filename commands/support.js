4ce321f0ac939fbc7f9ae59f0a111ab1f17aa392
shibihh <shibilka05@gmail.com>
2025-04-19 05:28:28 +0530

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -35,7 +35,7 @@ module.exports = {
             .setAuthor({
                 name: 'Support Server',
                 iconURL: Icons.dotIcon,
-                url: 'https://discord.gg/xQF9f9yUEM'
+                url: 'https://discord.gg/uc-1111272495497101372'
             })
             .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ `)
             .setImage('https://cdn.discordapp.com/attachments/1208349675250389032/1362931534298087445/standard.gif?ex=68043067&is=6802dee7&hm=5e65fec14822e005b04e0ad4b5a436121f92ee6bca0389053364b9dc44b8f59c&')
